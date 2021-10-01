5855ecc30cc51206fecadc541595e9f4908910a7
Tarang <tarangnair98@gmail.com>
2021-10-01 18:10:17 +0530

    Basic initialization

---
+++ app/src/main/java/com/example/camera2_api/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.camera2_api;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.hardware.camera2.CameraCaptureSession;
+import android.hardware.camera2.CameraDevice;
+import android.hardware.camera2.CaptureRequest;
+import android.os.Bundle;
+import android.util.SparseIntArray;
+import android.view.Surface;
+import android.view.TextureView;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
