967cab832c0fd974ab2ad85c0716a9a31573f017
Tarang <tarangnair98@gmail.com>
2021-10-03 00:42:29 +0530

    Getting the camera Id

--- app/src/main/java/com/example/camera2_api/MainActivity.java
+++ app/src/main/java/com/example/camera2_api/MainActivity.java
@@ -3,9 +3,13 @@ package com.example.camera2_api;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.graphics.SurfaceTexture;
+import android.hardware.camera2.CameraAccessException;
 import android.hardware.camera2.CameraCaptureSession;
+import android.hardware.camera2.CameraCharacteristics;
 import android.hardware.camera2.CameraDevice;
+import android.hardware.camera2.CameraManager;
 import android.hardware.camera2.CaptureRequest;
 import android.os.Bundle;
 import android.util.SparseIntArray;
@@ -23,6 +27,7 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public void onSurfaceTextureAvailable(@NonNull SurfaceTexture surface, int width, int height) {
             Toast.makeText(getApplicationContext(),"Texture View available",Toast.LENGTH_SHORT).show();
+            setUpCamera(width,height);
         }
 
         @Override
@@ -62,6 +67,8 @@ public class MainActivity extends AppCompatActivity {
         }
     };
 
+    private String cameraId;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -75,8 +82,11 @@ public class MainActivity extends AppCompatActivity {
     protected  void onResume(){
         super.onResume();
 
-        if(textureView.isAvailable()){
+        Toast.makeText(getApplicationContext(),"Resumed",Toast.LENGTH_SHORT).show();
 
+
+        if(textureView.isAvailable()){
+            setUpCamera(textureView.getWidth(),textureView.getHeight());
         }
         else{
             textureView.setSurfaceTextureListener(surfaceTextureListener);
@@ -86,6 +96,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onPause(){
         super.onPause();
+
+        Toast.makeText(getApplicationContext(),"Paused",Toast.LENGTH_SHORT).show();
+
 
         closeCamera();
     }
@@ -105,6 +118,22 @@ public class MainActivity extends AppCompatActivity {
                     | View.SYSTEM_UI_FLAG_FULLSCREEN
                     | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
             );
+        }
+    }
+
+    private void setUpCamera(int width, int height){
+        CameraManager cameraManager = (CameraManager)getSystemService(Context.CAMERA_SERVICE);
+        try {
+            for(String id : cameraManager.getCameraIdList()){
+                CameraCharacteristics cameraCharacteristics = cameraManager.getCameraCharacteristics(id);
+                if(cameraCharacteristics.get(CameraCharacteristics.LENS_FACING)== CameraCharacteristics.LENS_FACING_FRONT){
+                    continue;
+                }
+                cameraId = id;
+                return;
+            }
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
         }
     }
 
