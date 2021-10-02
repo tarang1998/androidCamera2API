edaefe9e9bddd5fbc22c8e6b4fd17b1c9fad3902
Tarang <tarangnair98@gmail.com>
2021-10-02 21:49:40 +0530

    Created the texture view

--- app/src/main/java/com/example/camera2_api/MainActivity.java
+++ app/src/main/java/com/example/camera2_api/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.camera2_api;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.SurfaceTexture;
 import android.hardware.camera2.CameraCaptureSession;
 import android.hardware.camera2.CameraDevice;
 import android.hardware.camera2.CaptureRequest;
@@ -11,13 +13,52 @@ import android.view.Surface;
 import android.view.TextureView;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextureView textureView;
+    private TextureView.SurfaceTextureListener surfaceTextureListener = new TextureView.SurfaceTextureListener() {
+
+        @Override
+        public void onSurfaceTextureAvailable(@NonNull SurfaceTexture surface, int width, int height) {
+            Toast.makeText(getApplicationContext(),"Texture View available",Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onSurfaceTextureSizeChanged(@NonNull SurfaceTexture surface, int width, int height) {
+
+        }
+
+        @Override
+        public boolean onSurfaceTextureDestroyed(@NonNull SurfaceTexture surface) {
+            return false;
+        }
+
+        @Override
+        public void onSurfaceTextureUpdated(@NonNull SurfaceTexture surface) {
+
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textureView = (TextureView) findViewById(R.id.textureView);
+    }
+
+    @Override
+    protected  void onResume(){
+        super.onResume();
+
+        if(textureView.isAvailable()){
+
+        }
+        else{
+            textureView.setSurfaceTextureListener(surfaceTextureListener);
+        }
     }
 
     @Override
